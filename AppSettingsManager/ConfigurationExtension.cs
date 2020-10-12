8bedce8d9f9dd2277ce3d492ccfc818031a3c99e
Ben Spark <bhrugen1990@gmail.com>
2020-10-12 17:35:14 -0500

    Section 3 - Extension Method

---
+++ AppSettingsManager/ConfigurationExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppSettingsManager
+{
+    public static class ConfigurationExtension
+    {
+        public static void AddConfiguration<T>(this IServiceCollection services,IConfiguration configuration,
+            string configurationTag=null) where T : class
+        {
+            if (string.IsNullOrEmpty(configurationTag))
+            {
+                configurationTag = typeof(T).Name;
+            }
+
+            var instance = Activator.CreateInstance<T>();
+            new ConfigureFromConfigurationOptions<T>(configuration.GetSection(configurationTag)).Configure(instance);
+            services.AddSingleton(instance);
+        }
+    }
+}
--- AppSettingsManager/Startup.cs
+++ AppSettingsManager/Startup.cs
@@ -25,9 +25,7 @@ namespace AppSettingsManager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var twilioSettings = new TwilioSettings();
-            new ConfigureFromConfigurationOptions<TwilioSettings>(Configuration.GetSection("Twilio")).Configure(twilioSettings);
-            services.AddSingleton(twilioSettings);
+            services.AddConfiguration<TwilioSettings>(Configuration, "Twilio");
             services.Configure<TwilioSettings>(Configuration.GetSection("Twilio"));
             services.AddControllersWithViews();
         }
