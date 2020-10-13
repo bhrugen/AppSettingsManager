476c62584b3a5b27266588552d26466cf46c3a50
Ben Spark <bhrugen1990@gmail.com>
2020-10-13 18:10:23 -0500

    Section 4 - Custom Json File

--- AppSettingsManager/Program.cs
+++ AppSettingsManager/Program.cs
@@ -21,11 +21,14 @@ namespace AppSettingsManager
             .ConfigureAppConfiguration((hostingContext,builder) => {
                 builder.Sources.Clear();
                 builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                
                 builder.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                builder.AddJsonFile("customJson.json", optional: true, reloadOnChange: true);
                 if (hostingContext.HostingEnvironment.IsDevelopment())
                 {
                     builder.AddUserSecrets<Program>();
                 }
+                
                 builder.AddEnvironmentVariables();
                 builder.AddCommandLine(args);
 
